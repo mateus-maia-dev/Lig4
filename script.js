900ea8d572a3dc701c60471624c940f4c97ff987
mateus maia <eng.fonsecamateus@gmail.com>
2021-01-05 12:52:41 -0300

    Começando JS. Terminar depois

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//teste para commit de branch
-// Hello World
+const tabuleiro = document.getElementById("board");
+
+for (let i = 1; i <= 7; i++) {
+  const columns = document.createElement("div");
+  columns.id = `col${i}`;
+  columns.classList.add("columns");
+
+  tabuleiro.appendChild(columns);
+}
+
+document.body.appendChild(tabuleiro);
+
+const onClick = (ev) => {
+  let coluna = document.getElementById(ev.target.id);
+  console.log(coluna);
+};
+
+tabuleiro.addEventListener("click", onClick);
